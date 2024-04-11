c9232846ec02342b3ad74e7dadad0e48a1a77c3a
ddong8 <1413470138@qq.com>
2024-04-11 15:27:11 +0800

    add doc page.

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,9 +1,35 @@
-import { title } from "@/components/primitives";
+"use client";
+import React, { useState } from "react";
+import { Button } from "@nextui-org/react";
 
-export default function DocsPage() {
-	return (
-		<div>
-			<h1 className={title()}>Docs</h1>
-		</div>
-	);
+function MyComponent() {
+  const [data, setData] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  // 处理按钮点击事件
+  const handleClick = async () => {
+    setIsDisabled(true); // 设置按钮状态为disable
+    try {
+      // 使用fetch发送请求
+      const resp = await fetch("https://restapi.amap.com/v3/ip");
+      const jsonData = await resp.json();
+      setData(jsonData); // 设置数据状态
+    } catch (error) {
+      console.error(error); // 打印失败信息
+    } finally {
+      setIsDisabled(false); // 请求结束，恢复按钮状态为enable
+    }
+  };
+
+  return (
+    <div>
+      <Button isDisabled={isDisabled} onClick={handleClick} color="primary">
+        发送请求
+      </Button>
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}{" "}
+      {/* 显示获取的数据 */}
+    </div>
+  );
 }
+
+export default MyComponent;
