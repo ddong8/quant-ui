1408880703ad8192b6c08d81f59f3aa9fd46d84c
ddong8 <1413470138@qq.com>
2024-04-15 23:17:36 +0800

    update api.

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,30 +1,68 @@
 "use client";
 import React, { useState } from "react";
-import { Button } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
 
 function MyComponent() {
   const [data, setData] = useState(null);
-  const [isDisabled, setIsDisabled] = useState(false);
+  const [isDisabled1, setIsDisabled1] = useState(false);
+  const [isDisabled2, setIsDisabled2] = useState(false);
+  const [inputValue, setInputValue] = useState('');
 
-  // 处理按钮点击事件
-  const handleClick = async () => {
-    setIsDisabled(true); // 设置按钮状态为disable
+  const handleChange = (event:any) => {
+    setInputValue(event.target.value);
+  };
+
+  // 处理启动按钮点击事件
+  const handleTradeStartClick = async () => {
+    setIsDisabled1(true); // 设置按钮状态为disable
     try {
       // 使用fetch发送请求
-      const resp = await fetch("https://restapi.amap.com/v3/ip");
+      const resp = await fetch("/api/v1/future/start_task", {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
       const jsonData = await resp.json();
       setData(jsonData); // 设置数据状态
     } catch (error) {
       console.error(error); // 打印失败信息
     } finally {
-      setIsDisabled(false); // 请求结束，恢复按钮状态为enable
+      setIsDisabled1(false); // 请求结束，恢复按钮状态为enable
     }
   };
 
+    // 处理启动按钮点击事件
+    const handleUpdateTargetProfitClick = async () => {
+      setIsDisabled2(true); // 设置按钮状态为disable
+      try {
+        // 使用fetch发送请求
+        const resp = await fetch("/api/v1/future/update_target_price/"+inputValue,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
+        const jsonData = await resp.json();
+        setData(jsonData); // 设置数据状态
+      } catch (error) {
+        console.error(error); // 打印失败信息
+      } finally {
+        setIsDisabled2(false); // 请求结束，恢复按钮状态为enable
+      }
+    };
+
   return (
     <div>
-      <Button isDisabled={isDisabled} onClick={handleClick} color="primary">
-        发送请求
+      <Button isDisabled={isDisabled1} onClick={handleTradeStartClick} color="primary">
+        启动交易
+      </Button>
+      <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+      <Input type="email" label="Email" placeholder="Enter your email" value={inputValue} onChange={handleChange}/>
+    </div>
+      <Button isDisabled={isDisabled2} onClick={handleUpdateTargetProfitClick} color="primary">
+        更新盈利
       </Button>
       {data && <pre>{JSON.stringify(data, null, 2)}</pre>}{" "}
       {/* 显示获取的数据 */}
