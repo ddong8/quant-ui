b0c96951237dee3edb5eef21b298e3df2051dcd1
ddong8 <1413470138@qq.com>
2024-04-18 00:41:06 +0800

    add child component.

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React, { useState } from "react";
-import { Button, Input, Textarea } from "@nextui-org/react";
+import { Button, Textarea } from "@nextui-org/react";
+import { UpdateTask } from "./update-task";
 
 function MyComponent() {
   const [taskId, setTaskId] = useState("");
   const [isDisabled1, setIsDisabled1] = useState(false);
-  const [isDisabled2, setIsDisabled2] = useState(false);
 
   const [values, setValues] = useState({
     code: "",
@@ -18,14 +18,6 @@ function MyComponent() {
   });
 
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-  const handleChange = (event: any) => {
-    const { name, value } = event.target;
-    setValues({
-      ...values,
-      [name]: value,
-    });
-  };
 
   // 处理启动按钮点击事件
   const handleTradeCreateClick = async () => {
@@ -61,32 +53,10 @@ function MyComponent() {
         body: JSON.stringify({ task_id: taskId }, null, 2),
       });
       const jsonData = await resp.json();
-      setData(jsonData); // 设置数据状态
     } catch (error) {
       console.error(error); // 打印失败信息
     } finally {
       setIsDisabled1(false); // 请求结束，恢复按钮状态为enable
-    }
-  };
-
-  // 处理启动按钮点击事件
-  const handleUpdateTargetProfitClick = async () => {
-    setIsDisabled2(true); // 设置按钮状态为disable
-    try {
-      // 使用fetch发送请求
-      const resp = await fetch(`${API_URL}/api/v1/future/update_task_config`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ task_id: taskId, task_config: values }, null, 2),
-      });
-      const jsonData = await resp.json();
-      setValues(jsonData.data.task_config);
-    } catch (error) {
-      console.error(error); // 打印失败信息
-    } finally {
-      setIsDisabled2(false); // 请求结束，恢复按钮状态为enable
     }
   };
 
@@ -122,62 +92,7 @@ function MyComponent() {
           停止交易
         </Button>
       </div>
-      <Input
-        label="品种"
-        name="code"
-        variant="bordered"
-        value={values.code}
-        onChange={handleChange}
-      />
-      <Input
-        label="开仓方向"
-        name="direction"
-        variant="bordered"
-        value={values.direction}
-        onChange={handleChange}
-      />
-      <Input
-        label="起始价格"
-        name="target_price"
-        variant="bordered"
-        value={values.target_price}
-        onChange={handleChange}
-      />
-      <Input
-        label="增仓价差"
-        name="price_diff_step"
-        variant="bordered"
-        value={values.price_diff_step}
-        onChange={handleChange}
-      />
-      <Input
-        label="增仓量差"
-        name="volume_diff_step"
-        variant="bordered"
-        value={values.volume_diff_step}
-        onChange={handleChange}
-      />
-      <Input
-        label="目标盈利"
-        name="target_profit"
-        variant="bordered"
-        value={values.target_profit}
-        onChange={handleChange}
-      />
-      <Input
-        label="最大仓位"
-        name="max_position_ratio"
-        variant="bordered"
-        value={values.max_position_ratio}
-        onChange={handleChange}
-      />
-      <Button
-        isDisabled={isDisabled2}
-        onClick={handleUpdateTargetProfitClick}
-        color="primary"
-      >
-        更新任务参数
-      </Button>
+      <UpdateTask taskId={taskId} values={values} setValues={setValues} />
     </div>
   );
 }
