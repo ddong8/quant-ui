22774a2aff00c5d86cb23ecbcee1acccd222db80
ddong8 <1413470138@qq.com>
2024-05-11 10:45:49 +0800

    fix navbar.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -60,22 +60,6 @@ export const Navbar = () => {
 						<p className="font-bold text-inherit">ACME</p>
 					</NextLink>
 				</NavbarBrand>
-				<ul className="hidden lg:flex gap-4 justify-start ml-2">
-					{siteConfig.navItems.map((item) => (
-						<NavbarItem key={item.href}>
-							<NextLink
-								className={clsx(
-									linkStyles({ color: "foreground" }),
-									"data-[active=true]:text-primary data-[active=true]:font-medium"
-								)}
-								color="foreground"
-								href={item.href}
-							>
-								{item.label}
-							</NextLink>
-						</NavbarItem>
-					))}
-				</ul>
 			</NavbarContent>
 
 			<NavbarContent
