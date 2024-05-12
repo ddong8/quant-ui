ec90b026e5e828376b3cc9d57603d096e5349b6d
ddong8 <1413470138@qq.com>
2024-05-12 22:09:32 +0800

    update import.

--- components/user-profile.tsx
+++ components/user-profile.tsx
@@ -1,6 +1,5 @@
-import { Button } from "@nextui-org/react";
 import { auth } from "../app/api/auth";
-import { SignIn, SignOut } from "./auth-components";
+import { SignIn } from "./auth-components";
 import UserButton from "./user-button";
 
 export default async function UserProfile() {
@@ -9,10 +8,6 @@ export default async function UserProfile() {
   return (
     <div className="flex gap-2 items-center">
       <UserButton session={session} />
-      {/* <Button variant="ghost" className="relative w-8 h-8 rounded-full">
-        {session.user.name ?? ""}
-      </Button> */}
-      {/* <SignOut /> */}
     </div>
   );
 }
