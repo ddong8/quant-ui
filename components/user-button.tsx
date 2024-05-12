6892779bedc953eb63840be5e4c758f52d1ae7cc
ddong8 <1413470138@qq.com>
2024-05-12 21:15:39 +0800

    update.

--- components/user-button.tsx
+++ components/user-button.tsx
@@ -12,7 +12,8 @@ import {
 import { useSession } from "next-auth/react";
 
 export default function UserButton() {
-  const { data: session, update } = useSession();
+  const { data: session } = useSession();
+  if (!session?.user) return null;
   return (
     <div className="flex gap-2 items-center">
       <Dropdown>
