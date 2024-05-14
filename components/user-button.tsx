768cc5fd6ceb235012d046179c1e2d6f1fdd0402
ddong8 <1413470138@qq.com>
2024-05-15 01:12:02 +0800

    update sign out.

--- components/user-button.tsx
+++ components/user-button.tsx
@@ -10,9 +10,13 @@ import {
   Button,
 } from "@nextui-org/react";
 import { Avatar } from "@nextui-org/react";
-import { SignOut } from "./auth-components";
 
-export default function UserButton({ session }: { session?: any }) {
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
+
+export default function UserButton() {
+  const { data: session } = useSession();
+  console.log(session);
   if (!session?.user) return null;
   return (
     <div className="flex gap-2 items-center">
@@ -41,8 +45,8 @@ export default function UserButton({ session }: { session?: any }) {
             </DropdownItem>
           </DropdownSection>
           <DropdownSection title="SignOut" aria-label="">
-            <DropdownItem key="SignOut" className="text-danger" color="danger">
-              <SignOut />
+            <DropdownItem key="SignOut" className="text-danger" color="danger" onPress={() => signOut()}>
+              Sign Out
             </DropdownItem>
           </DropdownSection>
         </DropdownMenu>
--- components/user-profile.tsx
+++ components/user-profile.tsx
@@ -5,11 +5,11 @@ import UserButton from "./user-button";
 import { useSession } from "next-auth/react";
 
 export default function UserProfile() {
-  const session = useSession();
+  const { data: session } = useSession();
   if (!session) return <SignIn />;
   return (
     <div className="flex gap-2 items-center">
-      <UserButton session={session} />
+      <UserButton />
     </div>
   );
 }
