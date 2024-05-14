88b0084724cbce39fc1a11fcf6d301efbe8d4b71
ddong8 <1413470138@qq.com>
2024-05-15 01:13:10 +0800

    remove log.

--- components/user-button.tsx
+++ components/user-button.tsx
@@ -16,7 +16,6 @@ import { signOut } from "next-auth/react";
 
 export default function UserButton() {
   const { data: session } = useSession();
-  console.log(session);
   if (!session?.user) return null;
   return (
     <div className="flex gap-2 items-center">
@@ -45,7 +44,12 @@ export default function UserButton() {
             </DropdownItem>
           </DropdownSection>
           <DropdownSection title="SignOut" aria-label="">
-            <DropdownItem key="SignOut" className="text-danger" color="danger" onPress={() => signOut()}>
+            <DropdownItem
+              key="SignOut"
+              className="text-danger"
+              color="danger"
+              onPress={() => signOut()}
+            >
               Sign Out
             </DropdownItem>
           </DropdownSection>
